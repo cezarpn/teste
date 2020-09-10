2c5a5d28d82169b9bb27a86a004f81fb5efdb256
cezarpn <70606928+cezarpn@users.noreply.github.com>
2020-09-10 09:58:07 -0300

    Manipulação de Dados 1

--- Trabalho_1/data/teste.py
+++ Trabalho_1/data/teste.py
@@ -2,7 +2,7 @@ refArquivo = open('D:\Downloads\TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_Annota
 arquivoFasta = refArquivo.read()
 cabecalho = arquivoFasta.split('\n')[0][1:]
 sequencia = ' '.join(arquivoFasta.split('\n')[1:])
-print("Identificador:%s"%cabecalho)
+print("Identificador: %s"%cabecalho)
 print('Sequencia: %s'%sequencia)
 refArquivo.close()
 
