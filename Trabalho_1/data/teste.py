c74109bec216a77139764cdae56767bc2ce83b33
cezarpn <70606928+cezarpn@users.noreply.github.com>
2020-09-10 09:49:40 -0300

    teste.py

--- Trabalho_1/data/teste.py
+++ Trabalho_1/data/teste.py
@@ -1,4 +1,9 @@
-a= 1+1
-b=3*3
-c= a + b
-print(c)
+refArquivo = open('D:\Downloads\TriTrypDB-47_TcruziCLBrenerEsmeraldo-like_AnnotatedProteins.fasta')
+arquivoFasta = refArquivo.read()
+cabecalho = arquivoFasta.split('\n')[0][1:]
+sequencia = ' '.join(arquivoFasta.split('\n')[1:])
+print("Identificador:%s"%cabecalho)
+print('Sequencia: %s'%sequencia)
+refArquivo.close()
+
+
